a4a108c2e8460864d8edf8389ef679fee5613c2c
sebastianbeltran142 <96145326+sebastianbeltran142@users.noreply.github.com>
2022-06-13 09:04:04 -0500

    Calculadora V1

---
+++ Calculator_VB_adb/Form1.Designer.vb
@@ -0,0 +1,359 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.pantalla = New System.Windows.Forms.TextBox()
+        Me.CE = New System.Windows.Forms.Button()
+        Me.flecha_rigth = New System.Windows.Forms.Button()
+        Me.porcent = New System.Windows.Forms.Button()
+        Me.mas_minus = New System.Windows.Forms.Button()
+        Me.square = New System.Windows.Forms.Button()
+        Me.potencia = New System.Windows.Forms.Button()
+        Me.division = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.uno_equis = New System.Windows.Forms.Button()
+        Me.multiplicar = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.mas = New System.Windows.Forms.Button()
+        Me.minus = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.igual = New System.Windows.Forms.Button()
+        Me.point = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'pantalla
+        '
+        Me.pantalla.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pantalla.Font = New System.Drawing.Font("Arial", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.pantalla.ForeColor = System.Drawing.Color.Blue
+        Me.pantalla.Location = New System.Drawing.Point(23, 22)
+        Me.pantalla.Multiline = True
+        Me.pantalla.Name = "pantalla"
+        Me.pantalla.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.pantalla.Size = New System.Drawing.Size(359, 49)
+        Me.pantalla.TabIndex = 0
+        '
+        'CE
+        '
+        Me.CE.BackColor = System.Drawing.Color.Tomato
+        Me.CE.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.CE.Location = New System.Drawing.Point(23, 88)
+        Me.CE.Name = "CE"
+        Me.CE.Size = New System.Drawing.Size(67, 54)
+        Me.CE.TabIndex = 1
+        Me.CE.Text = "CE"
+        Me.CE.UseVisualStyleBackColor = False
+        '
+        'flecha_rigth
+        '
+        Me.flecha_rigth.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.flecha_rigth.Location = New System.Drawing.Point(96, 88)
+        Me.flecha_rigth.Name = "flecha_rigth"
+        Me.flecha_rigth.Size = New System.Drawing.Size(67, 54)
+        Me.flecha_rigth.TabIndex = 2
+        Me.flecha_rigth.Text = "←"
+        Me.flecha_rigth.UseVisualStyleBackColor = True
+        '
+        'porcent
+        '
+        Me.porcent.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.porcent.Location = New System.Drawing.Point(169, 88)
+        Me.porcent.Name = "porcent"
+        Me.porcent.Size = New System.Drawing.Size(67, 54)
+        Me.porcent.TabIndex = 3
+        Me.porcent.Text = "%"
+        Me.porcent.UseVisualStyleBackColor = True
+        '
+        'mas_minus
+        '
+        Me.mas_minus.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.mas_minus.Location = New System.Drawing.Point(242, 88)
+        Me.mas_minus.Name = "mas_minus"
+        Me.mas_minus.Size = New System.Drawing.Size(67, 54)
+        Me.mas_minus.TabIndex = 4
+        Me.mas_minus.Text = "+/-"
+        Me.mas_minus.UseVisualStyleBackColor = True
+        '
+        'square
+        '
+        Me.square.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.square.Location = New System.Drawing.Point(315, 88)
+        Me.square.Name = "square"
+        Me.square.Size = New System.Drawing.Size(67, 54)
+        Me.square.TabIndex = 5
+        Me.square.Text = "√"
+        Me.square.UseVisualStyleBackColor = True
+        '
+        'potencia
+        '
+        Me.potencia.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.potencia.Location = New System.Drawing.Point(315, 148)
+        Me.potencia.Name = "potencia"
+        Me.potencia.Size = New System.Drawing.Size(67, 54)
+        Me.potencia.TabIndex = 10
+        Me.potencia.Text = "^"
+        Me.potencia.UseVisualStyleBackColor = True
+        '
+        'division
+        '
+        Me.division.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.division.Location = New System.Drawing.Point(242, 148)
+        Me.division.Name = "division"
+        Me.division.Size = New System.Drawing.Size(67, 54)
+        Me.division.TabIndex = 9
+        Me.division.Text = "÷"
+        Me.division.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button9.Location = New System.Drawing.Point(169, 148)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(67, 54)
+        Me.Button9.TabIndex = 8
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button8.Location = New System.Drawing.Point(96, 148)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(67, 54)
+        Me.Button8.TabIndex = 7
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button7.Location = New System.Drawing.Point(23, 148)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(67, 54)
+        Me.Button7.TabIndex = 6
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'uno_equis
+        '
+        Me.uno_equis.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.uno_equis.Location = New System.Drawing.Point(315, 208)
+        Me.uno_equis.Name = "uno_equis"
+        Me.uno_equis.Size = New System.Drawing.Size(67, 54)
+        Me.uno_equis.TabIndex = 15
+        Me.uno_equis.Text = "1/x"
+        Me.uno_equis.UseVisualStyleBackColor = True
+        '
+        'multiplicar
+        '
+        Me.multiplicar.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.multiplicar.Location = New System.Drawing.Point(242, 208)
+        Me.multiplicar.Name = "multiplicar"
+        Me.multiplicar.Size = New System.Drawing.Size(67, 54)
+        Me.multiplicar.TabIndex = 14
+        Me.multiplicar.Text = "x"
+        Me.multiplicar.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button6.Location = New System.Drawing.Point(169, 208)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(67, 54)
+        Me.Button6.TabIndex = 13
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button5.Location = New System.Drawing.Point(96, 208)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(67, 54)
+        Me.Button5.TabIndex = 12
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button4.Location = New System.Drawing.Point(23, 208)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(67, 54)
+        Me.Button4.TabIndex = 11
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'mas
+        '
+        Me.mas.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.mas.Location = New System.Drawing.Point(315, 268)
+        Me.mas.Name = "mas"
+        Me.mas.Size = New System.Drawing.Size(67, 114)
+        Me.mas.TabIndex = 20
+        Me.mas.Text = "+"
+        Me.mas.UseVisualStyleBackColor = True
+        '
+        'minus
+        '
+        Me.minus.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.minus.Location = New System.Drawing.Point(242, 268)
+        Me.minus.Name = "minus"
+        Me.minus.Size = New System.Drawing.Size(67, 54)
+        Me.minus.TabIndex = 19
+        Me.minus.Text = "-"
+        Me.minus.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.Location = New System.Drawing.Point(169, 268)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(67, 54)
+        Me.Button3.TabIndex = 18
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(96, 268)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(67, 54)
+        Me.Button2.TabIndex = 17
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(23, 268)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(67, 54)
+        Me.Button1.TabIndex = 16
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'igual
+        '
+        Me.igual.BackColor = System.Drawing.Color.Tomato
+        Me.igual.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.igual.ForeColor = System.Drawing.Color.Black
+        Me.igual.Location = New System.Drawing.Point(242, 328)
+        Me.igual.Name = "igual"
+        Me.igual.Size = New System.Drawing.Size(67, 54)
+        Me.igual.TabIndex = 24
+        Me.igual.Text = "="
+        Me.igual.UseVisualStyleBackColor = False
+        '
+        'point
+        '
+        Me.point.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.point.Location = New System.Drawing.Point(169, 328)
+        Me.point.Name = "point"
+        Me.point.Size = New System.Drawing.Size(67, 54)
+        Me.point.TabIndex = 23
+        Me.point.Text = "."
+        Me.point.UseVisualStyleBackColor = True
+        '
+        'Button0
+        '
+        Me.Button0.Font = New System.Drawing.Font("Calibri", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button0.Location = New System.Drawing.Point(23, 328)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(140, 54)
+        Me.Button0.TabIndex = 21
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(406, 411)
+        Me.Controls.Add(Me.igual)
+        Me.Controls.Add(Me.point)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.mas)
+        Me.Controls.Add(Me.minus)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.uno_equis)
+        Me.Controls.Add(Me.multiplicar)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.potencia)
+        Me.Controls.Add(Me.division)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.square)
+        Me.Controls.Add(Me.mas_minus)
+        Me.Controls.Add(Me.porcent)
+        Me.Controls.Add(Me.flecha_rigth)
+        Me.Controls.Add(Me.CE)
+        Me.Controls.Add(Me.pantalla)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Calculator basic AdB"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pantalla As TextBox
+    Friend WithEvents CE As Button
+    Friend WithEvents flecha_rigth As Button
+    Friend WithEvents porcent As Button
+    Friend WithEvents mas_minus As Button
+    Friend WithEvents square As Button
+    Friend WithEvents potencia As Button
+    Friend WithEvents division As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents uno_equis As Button
+    Friend WithEvents multiplicar As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents mas As Button
+    Friend WithEvents minus As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents igual As Button
+    Friend WithEvents point As Button
+    Friend WithEvents Button0 As Button
+End Class
---
+++ Calculator_VB_adb/Form1.vb
@@ -0,0 +1,221 @@
+﻿Public Class Form1
+    Private memoria1 As Double = 0.0
+    Private memoria2 As Double = 0.0
+    Private signo As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button0_Click(sender As Object, e As EventArgs) Handles Button0.Click
+        pantalla.Text = pantalla.Text & "0"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        pantalla.Text = pantalla.Text & "1"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        pantalla.Text = pantalla.Text & "2"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        pantalla.Text = pantalla.Text & "3"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        pantalla.Text = pantalla.Text & "4"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        pantalla.Text = pantalla.Text & "5"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        pantalla.Text = pantalla.Text & "6"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        pantalla.Text = pantalla.Text & "7"
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        pantalla.Text = pantalla.Text & "8"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        pantalla.Text = pantalla.Text & "9"
+    End Sub
+
+    Private Sub mas_Click(sender As Object, e As EventArgs) Handles mas.Click
+        Try
+            If pantalla.Text <> "" Then
+                memoria1 = Val(pantalla.Text)
+                signo = "+"
+                pantalla.Clear()
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub minus_Click(sender As Object, e As EventArgs) Handles minus.Click
+        Try
+            If pantalla.Text <> "" Then
+                memoria1 = Val(pantalla.Text)
+                signo = "-"
+                pantalla.Clear()
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub multiplicar_Click(sender As Object, e As EventArgs) Handles multiplicar.Click
+        Try
+            If pantalla.Text <> "" Then
+                memoria1 = Val(pantalla.Text)
+                signo = "*"
+                pantalla.Clear()
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub division_Click(sender As Object, e As EventArgs) Handles division.Click
+        Try
+            If pantalla.Text <> "" Then
+                memoria1 = Val(pantalla.Text)
+                signo = "/"
+                pantalla.Clear()
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub square_Click(sender As Object, e As EventArgs) Handles square.Click
+        Try
+            If pantalla.Text <> "" Then
+                memoria1 = Val(pantalla.Text)
+                signo = "raiz"
+                pantalla.Clear()
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub potencia_Click(sender As Object, e As EventArgs) Handles potencia.Click
+        Try
+            If pantalla.Text <> "" Then
+                memoria1 = Val(pantalla.Text)
+                signo = "exponente"
+                pantalla.Clear()
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub porcent_Click(sender As Object, e As EventArgs) Handles porcent.Click
+        Try
+            If pantalla.Text <> "" Then
+                memoria1 = Val(pantalla.Text)
+                signo = "%"
+                pantalla.Clear()
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub CE_Click(sender As Object, e As EventArgs) Handles CE.Click
+        pantalla.Clear()
+        memoria1 = 0.0
+        memoria2 = 0.0
+        signo = String.Empty
+    End Sub
+
+    Private Sub flecha_rigth_Click(sender As Object, e As EventArgs) Handles flecha_rigth.Click
+        Try
+            Dim largo As Integer
+            largo = pantalla.Text.Length
+            pantalla.Text = Mid(pantalla.Text, 1, largo - 1)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub mas_minus_Click(sender As Object, e As EventArgs) Handles mas_minus.Click
+        Try
+            If pantalla.Text <> "" Then
+                pantalla.Text = pantalla.Text * (-1)
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub uno_equis_Click(sender As Object, e As EventArgs) Handles uno_equis.Click
+        Try
+            If pantalla.Text <> "" Then
+                pantalla.Text = 1 / pantalla.Text
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub point_Click(sender As Object, e As EventArgs) Handles point.Click
+        If pantalla.Text = "" Then
+            pantalla.Text = "0."
+        ElseIf punto(pantalla.Text) = False Then
+            pantalla.Text = pantalla.Text & "."
+        End If
+    End Sub
+
+    Private Function punto(ByVal cadena As String) As Boolean
+        Dim largo As Integer
+        Dim respuesta As Boolean = False
+        largo = cadena.Length
+        For i As Integer = 1 To largo Step 1
+            If Mid(cadena, i, 1) = "." Then
+                respuesta = True
+
+            End If
+        Next
+        Return respuesta
+    End Function
+
+    Private Sub igual_Click(sender As Object, e As EventArgs) Handles igual.Click
+        Try
+            If pantalla.Text <> "" And memoria1 <> 0.0 Then
+                memoria2 = pantalla.Text
+                Calculator_VB_adb()
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Private Sub Calculator_VB_adb()
+        Select Case signo
+            Case "+"
+                pantalla.Text = memoria1 + memoria2
+            Case "-"
+                pantalla.Text = memoria1 - memoria2
+            Case "*"
+                pantalla.Text = memoria1 * memoria2
+            Case "/"
+                pantalla.Text = memoria1 / memoria2
+            Case "raiz"
+                pantalla.Text = memoria2 ^ (1 / memoria1)
+            Case "exponente"
+                pantalla.Text = memoria1 ^ memoria2
+            Case "%"
+                pantalla.Text = memoria1 * memoria2 / 100
+            Case Else
+                MsgBox("Error")
+                End Select
+    End Sub
+End Class
